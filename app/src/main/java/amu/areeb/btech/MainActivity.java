57c81f57e7e4c7db009c09c20d49373cd90b4490
iamareebjamal <jamal.areeb@gmail.com>
2015-07-18 22:00:42 +0530

    Initial Commit : Added Source

---
+++ app/src/main/java/amu/areeb/btech/MainActivity.java
@@ -0,0 +1,329 @@
+package amu.areeb.btech;
+
+import android.app.*;
+import android.os.*;
+import android.widget.*;
+import android.view.*;
+import android.text.style.*;
+import android.graphics.drawable.*;
+import android.graphics.*;
+import android.text.*;
+import android.animation.*;
+import android.net.*;
+import android.content.*;
+import java.util.*;
+import android.util.*;
+import android.widget.TextView.*;
+import android.view.inputmethod.*;
+
+public class MainActivity extends Activity 
+{
+	Window window;
+	EditText fn;
+	EditText en;
+	RelativeLayout bg;
+	LinearLayout marksLayout;
+	
+	ProgressDialog pd;
+	Result mResult;
+	
+	String status;
+	
+	int RED = R.color.red, RED_DARK = R.color.red_dark, BLUE = R.color.blue, BLUE_DARK = R.color.blue_dark, GREEN = R.color.green, GREEN_DARK = R.color.green_dark, YELLOW = R.color.yellow, YELLOW_DARK = R.color.yellow_dark, ORANGE = R.color.orange, ORANGE_DARK = R.color.orange_dark, PURPLE = R.color.purple, PURPLE_DARK = R.color.purple_dark, TEAL = R.color.teal, GREY = R.color.grey;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+		
+        setContentView(R.layout.main);
+		
+		window = getWindow();
+		window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+		window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+		window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+		window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+		statusBarColor(PURPLE_DARK);
+		marksLayout = (LinearLayout) findViewById(R.id.marksLayout);
+		
+		fn = (EditText) findViewById(R.id.fnEdt);
+		en = (EditText) findViewById(R.id.enEdt);
+		
+		fn.setFilters(new InputFilter[]{new InputFilter.AllCaps(), new InputFilter.LengthFilter(8)});
+		en.setFilters(new InputFilter[]{new InputFilter.AllCaps(), new InputFilter.LengthFilter(6)});
+		bg = (RelativeLayout) findViewById(R.id.bg);
+		
+		pd = new ProgressDialog(this);
+		pd.setMessage("Getting Result");
+		pd.setCanceledOnTouchOutside(false);
+		pd.setCancelable(false);
+    }
+	
+	@Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+		outState.putString("status", getSuccessStatus());
+		if(mResult!=null){
+        	outState.putParcelable("result", mResult);
+		}
+    }
+	
+	@Override
+	protected void onRestoreInstanceState(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onRestoreInstanceState(savedInstanceState);
+		try{
+			String status = savedInstanceState.getString("status");
+			mResult=savedInstanceState.getParcelable("result");
+			if(mResult!=null){
+				manageResult(status);
+			}
+			
+		} catch (BadParcelableException e){
+			e.printStackTrace();
+		}
+	}
+
+	/*@Override
+	protected void onDestroy()
+	{
+		// TODO: Implement this method
+		super.onDestroy();
+		Toast.makeText(this, "Asta La Vista Baby", Toast.LENGTH_SHORT).show();
+	}*/
+	
+	
+	
+    public void setSuccessStatus(String result){
+		this.status = result;
+	}
+	
+	public String getSuccessStatus(){
+		return this.status;
+	}
+	
+	public void celebrateCPI(String CPI){
+		int BG = 0, ST = 0;
+		try{
+			float cpi = Float.parseFloat(CPI);
+			if (cpi >= 8.5){
+				BG = BLUE;
+				ST = BLUE_DARK;
+			} else if (cpi >= 5.259){
+				BG = GREEN;
+				ST = GREEN_DARK;
+			} else if (cpi >= 3.185){
+				BG = YELLOW;
+				ST = YELLOW_DARK;
+			} else if (cpi >= 2.402){
+				BG = ORANGE;
+				ST = ORANGE_DARK;
+			} else {
+				BG = RED;
+				ST = RED_DARK;
+			}
+		} catch (NumberFormatException e){
+			BG = RED;
+			ST = RED_DARK;
+		}
+		changeColor(BG);
+		statusBarColor(ST);
+	}
+	
+	public void changeColor(int color){
+		int colorFrom = ((ColorDrawable) bg.getBackground()).getColor();
+		int colorTo = getResources().getColor(color);
+		ValueAnimator bgAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), colorFrom, colorTo);
+		bgAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener(){
+			@Override
+			public void onAnimationUpdate(ValueAnimator animator){
+				bg.setBackgroundColor((int)animator.getAnimatedValue());
+			}
+		});
+		
+		
+		bgAnimation.setDuration(2000).start();
+	}
+	
+	public void statusBarColor(int color){
+		if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP){
+			int colorStatusFrom = window.getStatusBarColor();
+			int colorStatusTo = getResources().getColor(color);
+			ValueAnimator stAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), colorStatusFrom, colorStatusTo);
+			stAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener(){
+					@Override
+					public void onAnimationUpdate(ValueAnimator animator){
+						window.setStatusBarColor((int)animator.getAnimatedValue());
+						window.setNavigationBarColor((int)animator.getAnimatedValue());
+					}
+				});
+
+			stAnimation.setDuration(2000).start();
+		}
+	}
+	
+	public void removeCards(){
+		marksLayout.removeAllViews();
+	}
+	
+	public RelativeLayout addCard(String info, String desc){
+		RelativeLayout card = (RelativeLayout) getLayoutInflater().from(this).inflate(R.layout.card_layout, null);
+		TextView inf = (TextView) card.findViewById(R.id.infTv);
+		TextView des = (TextView) card.findViewById(R.id.desTv);
+		inf.setText(info);
+		des.setText(desc);
+		marksLayout.addView(card);
+		return card;
+	}
+	
+	public RelativeLayout addSubjectCard(String info, String desc, int color){
+		RelativeLayout card = (RelativeLayout) getLayoutInflater().from(this).inflate(R.layout.card_layout, null);
+		TextView inf = (TextView) card.findViewById(R.id.infTv);
+		TextView des = (TextView) card.findViewById(R.id.desTv);
+		card.setAlpha(0.8f);
+		inf.setText(info);
+		inf.setTextColor(getResources().getColor(color));
+		des.setTypeface(null, Typeface.ITALIC);
+		des.setText(desc, BufferType.SPANNABLE);
+		Spannable s = (Spannable) des.getText();
+		s.setSpan(new ForegroundColorSpan(getResources().getColor(getGradeColor(desc))), desc.length()-4, desc.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+		marksLayout.addView(card);
+		return card;
+	}
+	
+	public void modify(RelativeLayout rl){
+		TextView tv = (TextView) rl.findViewById(R.id.desTv);
+		tv.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16);
+		tv.setTypeface(null, Typeface.ITALIC);
+	}
+	
+	public int getGradeColor(String mark){
+		String grade = mark.substring(mark.length()-4, mark.length()-2);
+		if (grade.contains("A")){
+			return BLUE;
+		} else if(grade.contains("B")){
+			return GREEN;
+		} else if(grade.contains("C")){
+			return YELLOW;
+		} else if(grade.contains("D")){
+			return ORANGE;
+		} else if(grade.contains("E")){
+			return RED;
+		}
+		return RED;
+	}
+	
+	public boolean isFacultyNumber(String fcNo){
+		if(fcNo.length()!=8){
+			return false;
+		}
+		String fYear = fcNo.substring(0,2);
+		String fBranch = fcNo.substring(2, 5);
+		String fRNo = fcNo.substring(5);
+		
+		if(TextUtils.isDigitsOnly(fYear)&&TextUtils.isDigitsOnly(fRNo)&&fBranch.matches("^[ACEKLMP][EKR][B]$+")&&Integer.parseInt(fYear)<=Integer.parseInt(String.valueOf(Calendar.getInstance().get(Calendar.YEAR)).substring(2))){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	public boolean isEnrolmentNumber(String enNo){
+		if(enNo.length()!=6){
+			return false;
+		}
+		String rgNo = enNo.substring(0,2);
+		String RNo = enNo.substring(2);
+
+		if(TextUtils.isDigitsOnly(RNo)&&rgNo.matches("^[FG][B-Z]$+")){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	public void getResult(View view){
+		mResult = new Result();
+		InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+		imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+		String enNo = en.getText().toString();
+		String facNo = fn.getText().toString();
+		if(!isFacultyNumber(facNo)){
+			fn.setError("Invalid Faculty Number");
+			return;
+		}
+		if(!isEnrolmentNumber(enNo)){
+			en.setError("Invalid Enrolment Number");
+			return;
+		}
+		pd.show();
+		//bg.setBackgroundColor(Color.parseColor("#33b5e5"));
+		ConnectivityManager conn = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo net = conn.getActiveNetworkInfo();
+		if (net != null){
+			new GetResult().execute("http://ctengg.amu.ac.in/result_btech.php", enNo, facNo);
+		} else {
+			pd.dismiss();
+			Toast.makeText(this, "No Connection", Toast.LENGTH_SHORT).show();
+		}
+	}
+	
+	public void manageResult(String result){
+		setSuccessStatus(result);
+		removeCards();
+		String cpi = "fail";
+		if (result.equals(Result.SUCCESS)){
+			cpi = mResult.getCPI();
+			String spi = mResult.getSPI();
+			String[] subs = mResult.getSubjects();
+			String[] marks = mResult.getMarks();
+			String name = mResult.getName();
+			addCard("Name : ", name);
+			String row = String.format("%s%8s%8s%8s", "Sessional", "Final", "Total", "Grade");
+			modify(addCard("Subject:", row));
+			int[] colors = {BLUE, RED, GREEN, ORANGE, YELLOW, PURPLE, TEAL, RED_DARK, ORANGE_DARK, GREEN_DARK, YELLOW_DARK, BLUE_DARK};
+			for(int i = 0; i < subs.length; i++){
+				addSubjectCard(subs[i] + " :", marks[i], colors[i]);
+			}
+			addCard("SPI : " + spi, "CPI : " + cpi);
+			
+		} else if (result.equals(Result.WRONG_INFO)){
+			addCard("The Enrolment Number or Faculty Number you entered is either wrong or doesn't match the database.\nPlease try again.", "");
+		} else if (result.equals(Result.NO_RESULT)){
+			addCard(Result.NO_RESULT, "");
+		} else if (result.equals(Result.TIMEOUT)){
+			addCard(Result.TIMEOUT, "");
+		} else if (result.equals(Result.UNKNOWN_ERROR)){
+			addCard(Result.UNKNOWN_ERROR, "");
+		} else if (result.contains(Result.EXCEPTION)){
+			Toast.makeText(this, result.substring(Result.EXCEPTION.length()), Toast.LENGTH_LONG).show();
+		} else {
+			Toast.makeText(this, "Unhandled Error: " + result, Toast.LENGTH_SHORT).show();
+		}
+		celebrateCPI(cpi);
+	}
+	
+	
+	
+	private class GetResult extends AsyncTask<String, Void, String>
+	{
+		
+		
+        @Override
+        protected String doInBackground(String... strings) {
+            
+            return mResult.getResultString(strings[0], strings[1], strings[2]);
+        }
+		
+        @Override
+        protected void onPostExecute(String s)
+		{
+            super.onPostExecute(s);
+			//Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+			pd.dismiss();
+			manageResult(s);
+        }
+	}
+}
+
+
---
+++ app/src/main/java/amu/areeb/btech/Result.java
@@ -0,0 +1,173 @@
+package amu.areeb.btech;
+import org.jsoup.Connection;
+import org.jsoup.nodes.*;
+import org.jsoup.*;
+import org.jsoup.select.*;
+import java.util.*;
+import java.net.*;
+import java.sql.*;
+import android.os.*;
+import android.text.*;
+
+public class Result implements Parcelable
+{
+
+	@Override
+	public int describeContents()
+	{
+		// TODO: Implement this method
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel p1, int p2)
+	{
+		// TODO: Implement this method
+	}
+	
+	
+	
+	private String name, CPI, SPI, subjects[], marks[];
+	public static String WRONG_INFO = "Faculty_No or En_No is incorrect!", NO_RESULT = "This Result has not been declared yet!", SUCCESS="Result downloaded successfully!", UNKNOWN_ERROR="Couldn't get result due to unknown Error!", TIMEOUT = "Timeout while connecting to website\nPlease check your connection and try again", EXCEPTION="iaj.Exception:";
+	
+	public Elements getTable(Document doc, int index){
+		return doc.select("table").get(index).select("tr");
+	}
+
+	public String getText(Element ele, String tag, int index){
+		return ele.select(tag).get(index).text();
+	}
+
+	private void setName(String name){
+		this.name = name;
+	}
+
+	public String getName(){
+		return this.name;
+	}
+
+	private void setCPI(String cpi){
+		this.CPI = cpi;
+	}
+
+	private void setSPI(String spi){
+		this.SPI = spi;
+	}
+
+	public String getCPI(){
+		return this.CPI;
+	}
+
+	public String getSPI(){
+		return this.SPI;
+	}
+
+	private void setSubjects(String[] subs){
+		this.subjects = subs;
+	}
+
+	private void setMarks(String[] marks){
+		this.marks = marks;
+	}
+
+	public String[] getSubjects(){
+		return this.subjects;
+	}
+
+	public String[] getMarks(){
+		return this.marks;
+	}
+	
+	private String toTitleCase(String givenString) {
+		
+		String[] arr = givenString.toLowerCase().split(" ");
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < arr.length; i++) {
+			sb.append(Character.toUpperCase(arr[i].charAt(0))).append(arr[i].substring(1)).append(" ");
+		}
+		return sb.toString().trim();
+	}
+	
+	private String fixMarks(String mark){
+		return mark.length()<2 ? TextUtils.isDigitsOnly(mark) ? "0" + mark : mark + "b" : mark;
+	}
+
+	public String getResultString(String url, String enNo, String facNo){
+		StringBuffer buffer = new StringBuffer();
+		try {
+			
+			Document doc  = Jsoup.connect(url).data("EN", enNo, "FN", facNo,"submit","submit").method(Connection.Method.POST).post();
+			String page = doc.toString();
+			if (doc.toString().contains(WRONG_INFO))
+			{
+				buffer.append(WRONG_INFO);
+			}
+			else if (page.contains(NO_RESULT)){
+				buffer.append(NO_RESULT);
+			}
+			else if (page.contains("CPI"))
+			{
+				Elements subTable = getTable(doc, 1);
+				Element infTable = getTable(doc, 2).get(1);
+				String name = getText(infTable, "th", 2);
+				String spi = getText(infTable, "th", 4);
+				String cpi = getText(infTable, "th", 5);
+				
+				setName(toTitleCase(name));
+				setSPI(spi);
+				setCPI(cpi);
+				List<String> mSub = new ArrayList<String>();
+				List<String> mMarks = new ArrayList<String>();
+				for (Element subs : subTable)
+				{
+					try
+					{
+						String sub = getText(subs, "td", 0);
+						String mark  = String.format("%s%11s%9s%10s  ", fixMarks(getText(subs, "td", 1)), fixMarks(getText(subs, "td", 2)), fixMarks(getText(subs, "td", 3)), getText(subs, "td", 5));
+						mSub.add(sub);
+						mMarks.add(mark);
+					}
+					catch (IndexOutOfBoundsException e)
+					{
+						e.printStackTrace();
+						continue;
+					}
+				}
+				if(mSub!=null&&mMarks!=null){
+					setMarks(mMarks.toArray(new String[mMarks.size()]));
+					setSubjects(mSub.toArray(new String[mSub.size()]));
+					buffer.append(SUCCESS);
+				}
+				else{
+					buffer.append(UNKNOWN_ERROR);
+				}
+			}
+			else
+			{
+				buffer.append(UNKNOWN_ERROR);
+			}
+
+		}
+		catch (SocketTimeoutException e){
+			buffer.append(TIMEOUT);
+		}
+		catch (Throwable t)
+		{
+			buffer.append(EXCEPTION+t.toString());
+			t.printStackTrace();
+		}
+		return buffer.toString();
+	}
+
+	@SuppressWarnings("unused")
+	public static final Parcelable.Creator<Result> CREATOR = new Parcelable.Creator<Result>(){
+		@Override
+		public Result createFromParcel(Parcel in) {
+			return new Result();
+		}
+		@Override
+		public Result[] newArray (int size) {
+			return new Result[size];
+		}
+	};
+}
